b17183932f5273bf68d2dc1d16c0f40f1185099d
Carlo Liparata <122491556+carlo-liparata@users.noreply.github.com>
2024-08-22 16:13:13 +0200

    Update CreateObjectMergeFromSelectedNodes.py
    fixed error which made the code fail when running on a node in a subnet

--- CreateObjectMergeFromSelectedNodes.py
+++ CreateObjectMergeFromSelectedNodes.py
@@ -7,15 +7,14 @@ for i in hou.selectedNodes():
     offset = hou.Vector2(2, -2)
     finalPos = hou.Vector2(ogPos[0]+offset[0], ogPos[1]+offset[1])
     
-    currentNodePath = "/obj/"+str(parent)+"/"+str(i)
-    geo = hou.node("/obj/"+str(parent))
-    
+    currentNodePath = i.path()
     i.setSelected(False,clear_all_selected=False)
     
-    objMerge = geo.createNode("object_merge")    
+    objMerge = parent.createNode("object_merge")    
     objMerge.move(finalPos)
     
     refObjParm = objMerge.parm("objpath1")
     refObjParm.set(currentNodePath)
     objMerge.setSelected(True, clear_all_selected=False)
     #print(currentNodePath)
+    
